423282fdf79ff5ad240b97b671ea57e14aae3801
ToxicKwas1221 <vano_2005@inbox.ru>
2020-02-18 01:25:37 +0100

    -c added. Minor fixes.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # hackertyper-cmd
 A terminal version of hackertyper. Inspired by https://hackertyper.net/
 # usage
-Put the code or file you want into the directory with `hackertyper.py`, then run:<br><br>
+* Running `python hackertyper.py` will randomly pick a file from `pwn3r-scripts` directory<br><br>
+* Use `-f or --file` flag to pick a specific file from the directory(place it there first). Example:<br><br>
 `python hackertyper.py -f yourfile.extension`<br><br>
-Or you can run `python hackertyper.py` which will randomly pick a file from `pwn3r-scripts` folder<br><br>
+* Use `-c or --color` flag to specify the color of the output. The options are: `black, red, green, yellow, blue, magenta, cyan, white. (Not case sensitive)` Example:<br><br>
+`python hackertyper.py -c magenta`<br><br>
+
 Then, start randomly smashing your keyboard like a trv hvck3r V3nd3ttv pwn3r.<br><br>
 Press `q` to stop the program.
 # credits
--- hackertyper.py
+++ hackertyper.py
@@ -6,52 +6,65 @@ from pynput.keyboard import Listener, KeyCode
 from colorama import Fore, Style
 
 if platform().lower().startswith('windows'):
-    from colorama import init
-    init()
+	from colorama import init
+	init()
 
 parser = ArgumentParser()
 parser.add_argument('-f', '--file', help='pick a specific file from the directory')
+parser.add_argument('-c', '--color', help='choose the output color. Options:\
+					black, red, green, yellow, blue, magenta, cyan, white. (Not case sensitive)'
+					, default='green')
 
 speed = 4  # characters per button press
 progress = 0
 
 
 def on_press(key):
-    global code, progress, speed
-    if key == KeyCode.from_char('q'):
-        return False  # Stops the listener thread
-    if '\n' in code[progress:progress+speed]:
-        print(code[progress:progress + speed], end='', flush=True)
-        progress += speed
-        while code[progress:progress+speed] == '    ':
-            print(code[progress:progress+speed], end='', flush=True)
-            progress += speed
-    else:
-        print(code[progress:progress+speed], end='', flush=True)
-        progress += speed
+	global code, progress, speed
+	if key == KeyCode.from_char('q'):
+		return False  # Stops the listener thread
+	if '\n' in code[progress:progress+speed]:
+		print(code[progress:progress + speed], end='', flush=True)
+		progress += speed
+		while code[progress:progress+speed] == '    ':
+			print(code[progress:progress+speed], end='', flush=True)
+			progress += speed
+	else:
+		print(code[progress:progress+speed], end='', flush=True)
+		progress += speed
 
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    if args.file:
-        try:
-            with open(args.file, 'r') as file:
-                code = file.read()
-        except FileNotFoundError:
-            print(Fore.RED+f'File {args.file} is not found!'+Fore.RESET)
-            exit()
-    else:
-        os.chdir('pwn3r-scripts')
-        file = random.choice(os.listdir(path='.'))
-        while file == '.DS_Store':  # MacOSX compatibility
-            file = random.choice(os.listdir(path='.'))
-        with open(file, 'r') as file:
-            code = file.read()
+	args = parser.parse_args()
+	color = args.color.upper()
 
-    print(Fore.GREEN, Style.BRIGHT)
-    listener = Listener(on_press=on_press, suppress=True)  # threading.Thread instance
-    listener.start()
-    listener.join()
-    print(Fore.RESET, Style.RESET_ALL)
+	print(Style.BRIGHT, end='')
+	if args.file:
+		try:
+			with open(args.file, 'r') as file:
+				code = file.read()
+		except FileNotFoundError:
+			print(Fore.RED+f'File {args.file} is not found!'+Style.RESET_ALL)
+			exit()
+	else:
+		os.chdir('pwn3r-scripts')
+		file = random.choice(os.listdir(path='.'))
+		while file == '.DS_Store':  # MacOSX compatibility
+			file = random.choice(os.listdir(path='.'))
+		with open(file, 'r') as file:
+			code = file.read()
+
+
+	try:
+		exec(f'print(Fore.{color}, end="")')
+	except (AttributeError, SyntaxError):
+		parser.print_help()
+		exit()
+
+
+	listener = Listener(on_press=on_press, suppress=True)  # threading.Thread instance
+	listener.start()
+	listener.join()
+	print(Style.RESET_ALL)
 
 # THE END
