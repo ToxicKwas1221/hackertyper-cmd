8461f0e8f14c3d8df9aa371cc23a270614c13adc
ToxicKwas1221 <vano_2005@inbox.ru>
2020-03-11 08:50:13 +0100

    -s and --bright added.

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # hackertyper-cmd
 A terminal version of hackertyper. Inspired by https://hackertyper.net/
 # usage
-* Running `python hackertyper.py` will randomly pick a file from `pwn3r-scripts` directory<br><br>
+* Run `python hackertyper.py` to randomly pick a file from `pwn3r-scripts` directory<br><br>
 * Use `-f or --file` flag to pick a specific file from the directory(place it there first). Example:<br><br>
 `python hackertyper.py -f yourfile.extension`<br><br>
 * Use `-c or --color` flag to specify the color of the output. The options are: `black, red, green, yellow, blue, magenta, cyan, white. (Not case sensitive)` Example:<br><br>
 `python hackertyper.py -c magenta`<br><br>
+* Use `-s or --speed` flag to set the speed of the output(the default is 4). Example:<br><br>
+`python hackertyper.py -s 6`
+* Use `--bright` flag to make the output bright.<br><br>
 
 Then, start randomly smashing your keyboard like a trv hvck3r V3nd3ttv pwn3r.<br><br>
 Press `esc` key to stop the program.
--- hackertyper.py
+++ hackertyper.py
@@ -4,38 +4,42 @@ from argparse import ArgumentParser
 from pynput.keyboard import Listener, KeyCode, Key
 from colorama import Fore, Style, init
 
-init()
+init()  # initialize colorama
 
 parser = ArgumentParser()
 parser.add_argument('-f', '--file', help='pick a specific file from the directory')
 parser.add_argument('-c', '--color', help='choose the output color. Options:\
 					black, red, green, yellow, blue, magenta, cyan, white. (Not case sensitive)'
 					, default='green')
+parser.add_argument('-s', '--speed', help='set the speed of the output(default is 4)', default=4, type=int)
+parser.add_argument('--bright', help='make the output bright', action='store_true')
 
-speed = 4  # characters per button press
-progress = 0
+_progress = 0
 
 
 def on_press(key):
-	global code, progress, speed
+	global code, _progress, speed
 	if key == Key.esc:
 		return False  # Stops the listener thread
-	if '\n' in code[progress:progress+speed]:
-		print(code[progress:progress + speed], end='', flush=True)
-		progress += speed
-		while code[progress:progress+speed] == '    ':
-			print(code[progress:progress+speed], end='', flush=True)
-			progress += speed
+	if '\n' in code[_progress:_progress+speed]:
+		print(code[_progress:_progress + speed], end='', flush=True)
+		_progress += speed
+		while code[_progress:_progress+speed] == '    ':
+			print(code[_progress:_progress+speed], end='', flush=True)
+			_progress += speed
 	else:
-		print(code[progress:progress+speed], end='', flush=True)
-		progress += speed
+		print(code[_progress:_progress+speed], end='', flush=True)
+		_progress += speed
 
 
 if __name__ == '__main__':
 	args = parser.parse_args()
+	speed = args.speed
 	color = args.color.upper()
 
-	print(Style.BRIGHT, end='')
+	if args.bright:
+		print(Style.BRIGHT, end='')
+		
 	if args.file:
 		try:
 			with open(args.file, 'r') as file:
@@ -46,18 +50,14 @@ if __name__ == '__main__':
 	else:
 		os.chdir('pwn3r-scripts')
 		file = random.choice(os.listdir(path='.'))
-		while file == '.DS_Store':  # MacOSX compatibility
-			file = random.choice(os.listdir(path='.'))
 		with open(file, 'r') as file:
 			code = file.read()
-
 
 	try:
 		exec(f'print(Fore.{color}, end="")')
 	except (AttributeError, SyntaxError):
 		parser.print_help()
 		exit()
-
 
 	listener = Listener(on_press=on_press, suppress=True)  # threading.Thread instance
 	listener.start()
